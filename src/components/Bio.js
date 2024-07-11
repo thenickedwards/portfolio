977a3d8bbefb2ceb53cc227ddce2f3e588661947
Nick Edwards <theadventuresofnickedwards@gmail.com>
2024-07-10 22:04:28 -0700

    Added title to h1s for hover and adjusted bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -30,51 +30,51 @@ export default function Bio() {
   return (
     <section className='about-container mt-4 col-10'>
         <div>
-          <p>I am a Data Engineer with a background in web development. With expertise in building and optimizing ETL pipelines using Python and SQL, I leverage my arts background to devise creative solutions, articulate clear documentation, and foster collaboration within my team. My experience includes ensuring data integrity, enhancing workflow efficiency, and implementing resource performance and error monitoring. Driven by a passion for sports data science, particularly in soccer, I love contributing to innovative projects and creating impactful, data-driven solutions.</p>
+          <p>As a Data Engineer and Backend Software Developer with a foundation in web development, I specialize in building and optimizing ETL pipelines using Python and SQL. My background in the arts equips me with a creative approach to problem-solving, clear communication skills, and a knack for fostering collaboration. I am passionate about sports data science, particularly soccer, and thrive on creating innovative, data-driven solutions that make a tangible impact.</p>
           
           <ColoredLine color="gray" />
 
           <h4 className='nav-main nav-menu' style={{fontWeight: '700'}}>toolkit</h4>
           <div className='tools-container col-10'>
             
-            <h1><SiPython alt="Python"/></h1>
-            <h1><TbSql alt="SQL"/></h1>
-            <h1><SiPostgresql alt="PostgreSQL"/></h1>
-            <h1><SiApacheairflow alt="Apache Airflow"/></h1>
+            <h1><SiPython title="Python"/></h1>
+            <h1><TbSql title="SQL"/></h1>
+            <h1><SiPostgresql title="PostgreSQL"/></h1>
+            <h1><SiApacheairflow title="Apache Airflow"/></h1>
 
-            <h1><SiGooglecloud alt="Google Cloud Platform"/></h1>
-            <h1><SiGooglebigquery alt="BigQuery"/></h1>
-            <h1><MdStorage alt="Google Cloud Storage"/></h1>
-            <h1><HiIdentification alt="IAM - Identity & Access Management"/></h1>
+            <h1><SiGooglecloud title="Google Cloud Platform"/></h1>
+            <h1><SiGooglebigquery title="BigQuery"/></h1>
+            <h1><MdStorage title="Google Cloud Storage"/></h1>
+            <h1><HiIdentification title="IAM - Identity & Access Management"/></h1>
             
 
-            <h1><SiHtml5 alt="HTML"/></h1>
-            <h1><SiCss3 alt="CSS"/></h1>
-            <h1><SiJavascript alt="JS/Javascript"/></h1>
+            <h1><SiHtml5 title="HTML"/></h1>
+            <h1><SiCss3 title="CSS"/></h1>
+            <h1><SiJavascript title="JS/Javascript"/></h1>
 
-            <h1><SiReact alt="React"/></h1>
-            <h1><SiExpress alt="Express"/></h1>
-            <h1><SiNodedotjs alt="Node.js"/></h1>
-            <h1><SiMongodb alt="MongoDB"/></h1>
+            <h1><SiReact title="React"/></h1>
+            <h1><SiExpress title="Express"/></h1>
+            <h1><SiNodedotjs title="Node.js"/></h1>
+            <h1><SiMongodb title="MongoDB"/></h1>
 
-            {/* <h1><SiJquery alt="jQuery"/></h1> */}
-            <h1><SiMysql alt="MySQL"/></h1>
-            {/* <h1><SiSequelize alt="Sequelize"/></h1> */}
-            <h1><SiGraphql alt="GraphQL"/></h1>
-            {/* <h1><SiHandlebarsdotjs alt="Handlebars"/></h1> */}            
-            {/* <h1><SiJest alt="Jest"/></h1> */}
-            {/* <h1><SiInsomnia alt="Insomnia"/></h1> */}
-            {/* <h1><SiHeroku alt="Heroku"/></h1> */}
-            {/* <h1><SiApollographql alt="Apollo"/></h1> */}
+            {/* <h1><SiJquery title="jQuery"/></h1> */}
+            <h1><SiMysql title="MySQL"/></h1>
+            {/* <h1><SiSequelize title="Sequelize"/></h1> */}
+            <h1><SiGraphql title="GraphQL"/></h1>
+            {/* <h1><SiHandlebarsdotjs title="Handlebars"/></h1> */}            
+            {/* <h1><SiJest title="Jest"/></h1> */}
+            {/* <h1><SiInsomnia title="Insomnia"/></h1> */}
+            {/* <h1><SiHeroku title="Heroku"/></h1> */}
+            {/* <h1><SiApollographql title="Apollo"/></h1> */}
 
-            <h1><FaSquareGit alt="Git"/></h1>
+            <h1><FaSquareGit title="Git"/></h1>
 
 
           </div>
         </div>
 
         <div className='col-2 portfolio-picture'>
-          <img alt="Nick Edwards - Full Stack Dev" src={nickPic} />
+          <img title="Nick Edwards - Full Stack Dev" alt="Nick Edwards - Full Stack Dev" src={nickPic} />
         </div>
     </section>
   )
